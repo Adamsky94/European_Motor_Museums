661748a6962422a4be01c12638c4ad3c52311de1
Adamsky94 <a.kiss1994@gmail.com>
2020-11-05 21:53:32 +0000

    Added letterspacing function for regional id's

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
-  /* Code for animating the parallax section -- https://stackoverflow.com/questions/7672556/how-to-add-an-opacity-fading-effect-to-to-the-jquery-slidetoggle -- */  
+
+    /*letterspacing function*/
+    function spacing() {
+        $(this).animate({
+            letterSpacing: "+=5px"
+        })
+    }
+
+    function spacingoff() {
+        $(this).animate({
+            letterSpacing: "-=5px"
+        })
+    }
+
+    const ids = $("#east, #north, #south, #west");
+
+     ids.mouseenter(spacing).mouseleave(spacingoff).css("cursor", "pointer");
+
+
+  /* Code for animating the parallax section -- https://stackoverflow.com/questions/7672556/how-to-add-an-opacity-fading-effect-to-to-the-jquery-slidetoggle -- */
+
+
   $("#east").mousedown(function(){
     $(".east").animate({
-            opacity: "toggle"
+            opacity: "toggle",
         }, "fast");
     });
 
@@ -23,7 +44,5 @@ $(document).ready(function(){
             opacity: "toggle"
         }, "fast");
     });
-
-    $("#east, #north, #south, #west").css("cursor", "pointer");
 
 });
