2a1afa55f95c976a81354400b797ccbf25166c35
Adamsky94 <a.kiss1994@gmail.com>
2020-11-08 16:44:29 +0000

    Added alert to response function

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -1,22 +1,24 @@
-
 /*EmailJS code segment inspired by the Interactive Frontend course material 
 https://github.com/Code-Institute-Solutions/InteractiveFrontendDevelopment-Resume/blob/master/03-SendingEmailsUsingEmailJS/06-sending_emails/assets/js/sendEmail.js*/
 
 function sendMail(modalContactForm) {
-    emailjs.send("EMMP","template_EMMP", {
-        "from_name": modalContactForm.name.value,
-        "from_email": modalContactForm.email.value,
-        "message": modalContactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            alert("Email sent successfully!");
-        },
-        function(error) {
-            console.log("FAILED", error);
-            alert("FAILED! "+error);
-        }
-    );
-    return false;  // To block from loading a new page
+    emailjs
+        .send("EMMP", "template_EMMP", {
+            from_name: modalContactForm.name.value,
+            from_email: modalContactForm.email.value,
+            message: modalContactForm.message.value,
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                modalContactForm.reset();
+                alert("Thank you for contacting us! We will shortly get back to you about your enquiry.");
+                $("#modalclose").click();
+            },
+            function (error) {
+                console.log("FAILED", error);
+                alert("Something went wrong. Please try again later!" + error);
+                $("#modalclose").click();
+            }
+        );
 }
