346c5e71511a1c338f7182f258430e705f1a9a22
Adamsky94 <a.kiss1994@gmail.com>
2020-11-13 13:53:38 +0000

    Added adjusted timing for landing text animation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,10 +12,7 @@ $(document).ready(function(){
          })
      }) 
 
-
-
   /* Code for animating the parallax section -- https://stackoverflow.com/questions/7672556/how-to-add-an-opacity-fading-effect-to-to-the-jquery-slidetoggle -- */
-
 
   $("#east").mousedown(function(){
     $(".east").animate({
@@ -40,5 +37,8 @@ $(document).ready(function(){
             opacity: "toggle"
         }, "fast");
     });
+    
+ /* Code from the animate.css documentation -- https://animate.style/#javascript */
+    document.documentElement.style.setProperty('--animate-duration', '2s');
 
 });
