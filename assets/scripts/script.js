a08b7194fc94b292dd727359149296f534e1b171
Adamsky94 <a.kiss1994@gmail.com>
2020-11-16 12:43:52 +0000

    Removed now obsolete collapse function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
 
-    /*collapse logic*/
-    const idArray = ["east", "north", "south", "west"];
-
-    idArray.forEach((element) => {
-         document.getElementById(element).addEventListener("click", () => {
-              Array.from(document.getElementsByClassName(`collapse-data-${element}`)).forEach((element) => {
-              element.classList.add("collapsed");
-              element.classList.remove("show");
-              })
-         })
-     }) 
-
   /* Code for animating the parallax section -- https://stackoverflow.com/questions/7672556/how-to-add-an-opacity-fading-effect-to-to-the-jquery-slidetoggle -- */
 
   $("#east").mousedown(function(){
