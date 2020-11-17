47f8fb35692dd6a7c0e34dd23b8fd2362cbb43d4
Adamsky94 <a.kiss1994@gmail.com>
2020-11-17 15:30:48 +0000

    Removed now unused code for parallax animation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,30 +1,4 @@
 $(document).ready(function(){
-
-  /* Code for animating the parallax section -- https://stackoverflow.com/questions/7672556/how-to-add-an-opacity-fading-effect-to-to-the-jquery-slidetoggle -- */
-
-  $("#east").mousedown(function(){
-    $(".east").animate({
-            opacity: "toggle",
-        }, "fast");
-    });
-
-    $("#north").mousedown(function(){
-    $(".north").animate({
-            opacity: "toggle"
-        }, "fast");
-    });
-
-    $("#south").mousedown(function(){
-    $(".south").animate({
-            opacity: "toggle"
-        }, "fast");
-    });
-
-    $("#west").mousedown(function(){
-    $(".west").animate({
-            opacity: "toggle"
-        }, "fast");
-    });
     
     let map;
 
